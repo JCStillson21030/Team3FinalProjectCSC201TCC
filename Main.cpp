64248afa4683f797ea7f4da32cb51bdd2e46289b
Brandon Byrd <83299673+BMannyB@users.noreply.github.com>
2021-04-30 19:12:44 -0400

    Main
    Functions for final project. Blank.

---
+++ Main.cpp
@@ -0,0 +1,119 @@
+#include <iostream>
+using namespace std;
+
+void displayPersons(){
+    
+}
+
+void removePerson(){
+
+}
+
+void addPerson(){
+
+}
+
+void addBulk(){
+
+}
+
+void editPerson(){
+
+}
+
+void socialSecurity(){
+
+}
+
+void cardioDisease(){
+
+}
+
+void genderRatio(){
+
+}
+
+void findParents(){
+
+}
+
+void findChildren(){
+
+}
+
+void findSiblings(){
+
+}
+
+void findRelatives(){
+
+}
+
+int main(){
+
+int choice;
+
+cout << "Choose a function." << endl << endl;
+
+//--------------Options------------ 
+cout << "  1)  Display persons. " << endl;
+cout << "  2)  Remove a person. " << endl;
+cout << "  3)  Add a person. " << endl;
+cout << "  4)  Add new people in bulk by reading supplementary file.  " << endl;
+cout << "  5)  Edit a person. " << endl;
+cout << "  6)  Display those eligible for social security (65+). " << endl;
+cout << "  7)  Display all persons at high risk for cardiovascular disease. (BMI 27+) " << endl;
+cout << "  8)  Display male:female ratio. " << endl;
+cout << "  9)  Find parents of a person. " << endl;
+cout << " 10) Find children of a person. " << endl;
+cout << " 11) Find siblings of a person. " << endl;
+cout << " 12) Find uncles, aunts, cousins, nephews, and nieces of a person. " << endl;
+//---------------------------------
+
+cin >> choice;
+
+//-------Function List----------
+
+if (choice = 1){
+    displayPersons();
+}
+if (choice = 2){
+    removePerson();
+}
+if (choice = 3){
+    addPerson();
+}
+if (choice = 4){
+    addBulk();
+}
+if (choice = 5){
+    editPerson();
+}
+if (choice = 6){
+    socialSecurity();
+}
+if (choice = 7){
+    cardioDisease();
+}
+if (choice = 8){
+    genderRatio();
+}
+if (choice = 9){
+    findParents();
+}
+if (choice = 10){
+    findChildren();
+}
+if (choice = 11){
+    findSiblings();
+}
+if (choice = 12){
+    findRelatives();
+}
+
+return 0;
+}
+
+
+
+
