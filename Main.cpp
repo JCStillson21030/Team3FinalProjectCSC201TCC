647bd86e98853ef35aac34a1c578df9e6dd07d49
Brandon Byrd <83299673+BMannyB@users.noreply.github.com>
2021-04-30 19:17:07 -0400

    Add struct
    Adds the struct Person

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 using namespace std;
 
+
+//------------Person Struct-----------
+struct person{  // define the UDT 
+    
+    string pName;
+    long pSSN; 
+
+    char gender; 
+    string DOB;
+    
+    float height; 
+    float weight;
+    
+    long fSSN;
+    long mSSN;
+//-------------------------------------    
+
+    person* next;
+};
+
+
 void displayPersons(){
     
 }
