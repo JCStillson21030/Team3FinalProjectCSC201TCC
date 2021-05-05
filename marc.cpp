1c561a58a5117b3889886587550dce92c5500620
MarcBruno <83302778+Junk4hire@users.noreply.github.com>
2021-05-05 08:26:55 -0400

    Added Picasso level art

--- marc.cpp
+++ marc.cpp
@@ -8,3 +8,22 @@ void welcomeMsg(){
 void goodbyeMsg(){
    cout << "Thank you for using the person directory using linked lists program created by: Jon Stillson, Jamin White, Brandon Burns, Brandon Byrd, and Marc Bruno " << endl;
 }
+
+void trollClass(){
+    std::cout << R"(
+ ⊂_ヽ
+　 ＼＼ ＿
+　　 ＼(　•_•) F
+　　　 <　⌒ヽ A
+　　　/ 　 へ＼ B
+　　 /　　/　＼＼ U
+　　 ﾚ　ノ　　 ヽ_つ L
+　　/　/ O
+　 /　/ U
+　(　(ヽ S
+　|　|、＼.
+　| 丿 ＼ ⌒)
+　| |　　) /
+ノ )　　 L ﾉ__
+(／___)" << '\n';
+}
