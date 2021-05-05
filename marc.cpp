cc5ff50054d4e6e3786263ef3276e290a979798d
MarcBruno <83302778+Junk4hire@users.noreply.github.com>
2021-05-05 08:06:56 -0400

    Welcome and goodbye messages

--- marc.cpp
+++ marc.cpp
@@ -1,1 +1,10 @@
-//in here bruno
+#include <iostream>
+using namespace std;
+
+void welcomeMsg(){ 
+   cout << "Welcome to the CSC201 Final Project person directory using linked lists made by: Jon Stillson, Jamin White, Brandon Burns, Brandon Byrd, and Marc Bruno " << endl;
+}
+
+void goodbyeMsg(){
+   cout << "Thank you for using the person directory using linked lists program created by: Jon Stillson, Jamin White, Brandon Burns, Brandon Byrd, and Marc Bruno " << endl;
+}
